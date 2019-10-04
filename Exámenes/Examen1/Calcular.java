0ce6b61358477f1e8d540a19570bf1f93e427a9d
Jair Escamilla <hernandez_yo14@outlook.es>
2019-10-03 21:48:56 -0500

    Examen 1 Listo

---
+++ Exámenes/Examen1/Calcular.java
@@ -0,0 +1,38 @@
+public class Calcular
+{
+  double Resultado;
+  public int[] Ordenar(int Array[])
+  {
+    int Temp;
+    for(int i=0; i<Array.length; i++)
+    {
+      for(int j=0; j<Array.length; j++)
+      {
+        if(Array[i] < Array[j])
+        {
+          Temp = Array[i];
+          Array[i] = Array[j];
+          Array[j] = Temp;
+        }
+      }
+    }
+    return Array;
+  }
+  public double ObtenerResultado(int NumOrdenados[], double Percentil)
+  {
+    double Posicion;
+    Posicion = ((60*Percentil)/100);
+    System.out.println(Posicion);
+    if(Posicion%1==0)
+    {
+      Resultado = NumOrdenados[(int)Posicion-1] + NumOrdenados[(int)Posicion];
+      Resultado /= 2;
+    }
+    else
+    {
+      Resultado = NumOrdenados[(int)Posicion];
+    }
+    return Resultado;
+
+  }  
+}
